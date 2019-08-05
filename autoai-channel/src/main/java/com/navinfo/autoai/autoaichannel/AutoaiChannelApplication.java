e9727c4bb17c4f46a4bf7c7382e63e27ce1ec2b7
yanghh <136158344@qq.com>
2019-08-05 11:42:04 +0800

    test1

--- autoai-channel/src/main/java/com/navinfo/autoai/autoaichannel/AutoaiChannelApplication.java
+++ autoai-channel/src/main/java/com/navinfo/autoai/autoaichannel/AutoaiChannelApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoaiChannelApplication {
 
     public static void main(String[] args) {
+        System.out.println("渠道项目启动成功..........................");
         SpringApplication.run(AutoaiChannelApplication.class, args);
     }
 
